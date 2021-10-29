74097a7b1af18c11b4cb702c746bc88a70adf178
david isaac Survive <davidisaacsurvive@gmail.com>
2021-10-29 17:10:16 +0100

    Register and Profile link added to navbar

--- base/models.py
+++ base/models.py
@@ -17,7 +17,7 @@ class Product(models.Model):
     )
     posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    image = models.ImageField()
+    # image = models.ImageField()
     amount = models.CharField(max_length=20)
     category = models.CharField(choices=CATEGORY, max_length=50)
     description = models.TextField(null=True, blank=True)
